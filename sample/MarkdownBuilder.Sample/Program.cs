1e5025e3aee545bb0594cd0589111ebc31f66849
Charles de Vandiere <charles.de.vandiere@outlook.fr>
2020-04-03 10:27:22 +0200

    add sample project

---
+++ sample/MarkdownBuilder.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkdownBuilder.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
